8b5661ba4f2a81ee7a66f77e9c148a79fa165853
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-22 01:27:14 +0900

    feat: 로그인한 유저 정보 가져오기(useMeAPI)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
+import { useMeAPI } from '@/apis/auth';
 
 export default function page() {
+  const { data, isLoading } = useMeAPI();
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+  if (!isLoading && data) {
+    console.log('data>>>', data);
+  }
+
   return <div>page</div>;
 }
